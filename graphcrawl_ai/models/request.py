5f64c8c2d0f248ab509c4b87850c73b0db142abd
Utsab Khadka <utsabkhadka9475@gmail.com>
2026-06-01 21:45:46 +0545

    feat: added model for extraction requst generated from user request.

--- graphcrawl_ai/models/request.py
+++ graphcrawl_ai/models/request.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, model_validator
 from enum import Enum
-from graphcrawl_ai.llm import prompts
+from graphcrawl_ai.llm.promt_resolver.url_prompt_resolver import resolve_prompt
 
 class QuickOption(str, Enum):
     SUMMARY = "summary"
@@ -8,35 +8,26 @@ class QuickOption(str, Enum):
     PRODUCTS = "products"
     AUTO = "auto"
 
-    @property
-    def option(self) -> str:
-        if self == QuickOption.SUMMARY:
-            return prompts.summary
-        if self == QuickOption.CONTACTS:
-            return prompts.contacts
-        if self == QuickOption.PRODUCTS:
-            return prompts.products
-        if self == QuickOption.AUTO:
-            return prompts.auto
-
-class ExtractionRequest(BaseModel):
+class ExtractionRequestByUser(BaseModel):
     """
     Represents extraction request made by user
 
-    This is a model representation of what user requestse even before it hits the fetcher
+    This model is a representation of what user requests even before it hits the fetcher
     includes the source and prompt/quick extraction(pre defined extraction options) 
     """
     source: str
     prompt: str | None = None
     quick_option: QuickOption | None = None
 
-    @model_validator(mode="after")
-    def get_prompt_from_option(self) -> ExtractionRequest:
-        
-        if self.prompt and self.quick_option:
-            self.quick_option = None
 
-        if self.quick_option and not self.prompt:
-            self.prompt = self.quick_option.option
+class ExtractionRequestToLLM(BaseModel):
+    """
+    Represents extraction request being sent to LLM for scraping from url
 
-        return self
+    This model is a representation of the actual thing LLM needs to actually scrape
+     - content (which is clean text parsed from HTML fetched from the requsted url.)
+     - prompt (prompt that guides LLM for scraping as per user requirement.)
+    """
+    content: str
+    prompt: str
+
--- tests/test_init.py
+++ tests/test_init.py
@@ -2,4 +2,4 @@ from graphcrawl_ai.extraction.extraction_request import request
 
 x = request(source="https://github.com/utsabkhadka0710",quick_option="products")
 
-print(x.get('prompt'))
+print(x.get('quick_option'))
