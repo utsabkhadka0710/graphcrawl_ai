9d4938c55404b42af194ae23927a2819d7eb20c7
Utsab Khadka <utsabkhadka9475@gmail.com>
2026-06-21 23:43:06 +0545

    refactor(params): added LLM related params in 'crawl_url()'                   + import fixes after initial migration to intructor+litellm
    Added new parameters on crawl_url() associated with LLM
     - model (stores the LLM model to be used for extraction/crawling)
     - api_key (param for API key if user wants to pass the api-key directly instead of environment variable)

--- graphcrawl_ai/crawler/crawl_url.py
+++ graphcrawl_ai/crawler/crawl_url.py
@@ -1,6 +1,6 @@
-from graphcrawl_ai.llm.request_resolver.user_url_request_resolver import resolve_url_request
+from graphcrawl_ai.resolver.url_resolver import resolve_url_request
 from graphcrawl_ai.models.crawl_url.request_models.user_request import UrlExtractionRequest, AvailableQuickOption
-from graphcrawl_ai.llm.gemini_extractor import get_response_gemini
+from graphcrawl_ai.llm.engine import llm_extract
 from typing import Literal, Optional, TypeVar, get_args
 from pydantic import BaseModel, ValidationError
 
@@ -16,12 +16,14 @@ CrawlUrlResponse = TypeVar("ResponseSchema",bound=BaseModel)
 
 def crawl_url(source: str = None,
               prompt: str = None, 
-              quick_option: AvailableQuickOption = None, 
+              quick_option: AvailableQuickOption = None,
+              model: str = None,
+              api_key: str = None,
+              response_schema: type[BaseModel] | None = None,
               llm_timeout: Optional[float] = 60,
               llm_retry: Optional[float] = 3,
               crawl_timeout: Optional[float] = 30, 
-              crawl_retry: Optional[float] = 3,
-              response_schema: type[BaseModel] | None = None 
+              crawl_retry: Optional[float] = 3
             ) -> CrawlUrlResponse:
     """
     Scrapes a webpage and extracts structured data using an AI model.
@@ -69,19 +71,21 @@ def crawl_url(source: str = None,
             source = source,
             prompt = prompt,
             quick_option = quick_option,
+            model = model,
+            api_key = api_key,
+            response_schema = response_schema,
             llm_timeout = safe_cast(llm_timeout, 'llm_timeout'),
             llm_retry = safe_cast(llm_retry, 'llm_retry'),
             crawl_timeout = safe_cast(crawl_timeout, 'crawl_timeout'),
-            crawl_retry = safe_cast(crawl_retry, 'crawl_retry'),
-            response_schema = response_schema
+            crawl_retry = safe_cast(crawl_retry, 'crawl_retry')
         )
     except ValidationError as e:
         err_msg = e.errors()
         failed_param = err_msg[0]["loc"][0]
         raise InvalidDataError(param_name=failed_param)
     
-    request_to_llm, response_schema = resolve_url_request(request=request_by_user)
+    request_to_llm = resolve_url_request(request=request_by_user)
     
-    response_from_llm = get_response_gemini(request=request_to_llm, response_schema=response_schema)
+    response_from_llm = llm_extract(request=request_to_llm)
 
     return response_from_llm
