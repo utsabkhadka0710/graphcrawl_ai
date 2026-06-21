7214c6a12b9923172d94e6d7d22e647018216323
Utsab Khadka <utsabkhadka9475@gmail.com>
2026-06-21 23:33:17 +0545

    feat(exceptions): initialize custom exceptions for llm engine
    Added custom exception for GraphCrawlAI LLM engine
     - LLMError #Base exception class for error related to llm
     - LLMAuthenticationError #Raised when providers API authentication fails
     - LLMTimeoutError  #Raised when timeout for LLM to generate response
     - LLMRetryError  #Raised when retried maximum time in case of failure that led to recalling LLM
     - LLMRateLimitError  #Raised when the providers returns the rate-limit error
     - LLMContextWindowExceededError  #Raised when the content is larger than LLM context
     - LLMUnavailabeError  #Raised when the providers return service unavailable
     - APIError  #Raised when unexpected API error occurs
     - LLMUnknownError  #Raised when got unexpected error from the LLM provider

---
+++ graphcrawl_ai/exceptions/llm/llm_extractor_exceptions.py
@@ -0,0 +1,81 @@
+from abc import ABC
+
+class LLMError(Exception, ABC):
+    __module__ = "graphcrawl_ai"
+    pass
+
+class LLMAuthenticationError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "LLM provider Authentication failed! "
+                "Please check your API key (and API Base URL if provider requires)."
+            )
+        super().__init__(message)
+
+class LLMTimeoutError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "LLM Timeout! retried maximum times"
+            )
+        super().__init__(message)
+
+class LLMRetryError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "Ret"
+            )
+        super().__init__(message)
+
+class LLMRateLimitError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "LLM Rate-Limit! The provider raised rate-limit on this request. "
+                "Please try again later."
+            )
+        super().__init__(message)
+
+class LLMContextWindowExceededError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "LLM Context Window Exceeded!"
+            )
+        super().__init__(message)
+
+class LLMUnavailabeError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "LLM Unavailable! The provider service is currently unavailabe. "
+                "Please try again later."
+            )
+        super().__init__(message)
+
+class APIError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "An unexpected API Error occured. "
+                "Please check your API-key or contact your provider."
+            )
+        super().__init__(message)
+
+class LLMUnknownError(LLMError):
+    __module__ = "graphcrawl_ai"
+    def __init__(self, message:any = None):
+        if message is None:
+            message = (
+                "An unexpected error occured during LLM extraction!"
+            )
+        super().__init__(message)
